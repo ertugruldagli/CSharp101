4ad9500fa11701d952fee0757b92a5ee6d28c39d
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-26 20:38:14 +0300

    commit

--- Workout01/MultiplicationTable/Program.cs
+++ Workout01/MultiplicationTable/Program.cs
@@ -2,6 +2,12 @@
 {
     private static void Main(string[] args)
     {
-       
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int k=1; k<=10 ;k ++)
+            {
+
+            }
+        }
     }
 }
