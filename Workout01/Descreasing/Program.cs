c46d473c3d29b9a20ebf6344e1c1123e0ec8d970
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-26 15:56:38 +0300

    commit

--- Workout01/Descreasing/Program.cs
+++ Workout01/Descreasing/Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int girilenSayi,i;
+
+        Console.Write("Bir sayi giriniz: ");
+        girilenSayi = Convert.ToInt32(Console.ReadLine());
+
+        for ( i = 1; i < girilenSayi;) 
+        {
+            i = i - 5;
+            Console.WriteLine(i);
+        }
+        //Console.WriteLine(i);
+        Console.ReadKey();  
     }
 }
