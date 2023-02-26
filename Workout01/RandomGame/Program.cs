0b0ff72daf1547bdcbb3c9d8f0ed6ace35769572
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-26 21:48:20 +0300

    c

--- Workout01/RandomGame/Program.cs
+++ Workout01/RandomGame/Program.cs
@@ -1,7 +1,43 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        int randomSayi, tahminSayi;
 
+        Random rnd= new Random();
+        randomSayi=rnd.Next(5);
+
+        
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine("Bir sayi tahmin ediniz: ");
+            tahminSayi = Convert.ToInt32(Console.ReadLine());
+
+            if (randomSayi==tahminSayi)
+            {
+                Console.WriteLine("Tebrikler tahmin ettiniz!!");
+                break;
+            }
+            else 
+            {
+                Console.WriteLine(" YANLIS!! Tekrar deneyin");
+            }
+ 
+        }
+        
+
+
+
+
+
+
+
+
+
+
+        Console.ReadKey();
     }
 }
