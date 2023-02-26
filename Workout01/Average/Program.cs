4f8cd2b791ef833bf845bdcde4d8a84748c80c03
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-26 19:23:19 +0300

    average the commit

--- Workout01/Average/Program.cs
+++ Workout01/Average/Program.cs
@@ -1,20 +1,26 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int girilenSayi,toplam=0,adet=0;
-        double ort;
-
-        Console.Write("bir sayi giriniz: ");
-        girilenSayi = Convert.ToInt32(Console.ReadLine());
-
-        for ( int i = 1; i < girilenSayi ; i++)
+        int girilenSayi=1,toplam=0,adet=0; //girilenSayi değişkenine 1 atması yaptık. çünkü döngüde 0 ataması olduğunda döngü sona erecek.
+        float ort;
+        
+        while (girilenSayi!=0)   //girilen değer  sıfıra eşit olana kadar  döngü  çalışacak
         {
-            toplam += i;
+            Console.Write("bir sayi giriniz: ");
+            girilenSayi = Convert.ToInt32(Console.ReadLine());
+
             adet++;
+
+            toplam += girilenSayi;
+
         }
         ort = toplam / adet;
-        Console.WriteLine(ort);
+
+        Console.WriteLine("toplamı: " + toplam);
+        Console.WriteLine("ortalamasi: " + ort);
 
         Console.ReadKey();
 
