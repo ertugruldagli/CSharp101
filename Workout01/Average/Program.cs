a70a7da9b3c35ba01a81003ecec6fa5933889371
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-26 16:59:34 +0300

    commit

--- Workout01/Average/Program.cs
+++ Workout01/Average/Program.cs
@@ -2,6 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int girilenSayi,toplam=0,adet=0;
+        double ort;
+
+        Console.Write("bir sayi giriniz: ");
+        girilenSayi = Convert.ToInt32(Console.ReadLine());
+
+        for ( int i = 1; i < girilenSayi ; i++)
+        {
+            toplam += i;
+            adet++;
+         
+        }
+        ort = toplam / adet;
+        Console.WriteLine(ort);
+
+        Console.ReadKey();
+
     }
 }
