19a71a800ec7a4ad3970858ce0e2c5a96227424e
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-26 15:58:12 +0300

    avarage commitlendi

--- Workout01/Descreasing/Program.cs
+++ Workout01/Descreasing/Program.cs
@@ -2,15 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        int girilenSayi,i;
+        int girilenSayi,i=1;
 
         Console.Write("Bir sayi giriniz: ");
         girilenSayi = Convert.ToInt32(Console.ReadLine());
 
-        for ( i = 1; i < girilenSayi;) 
+        while (i<girilenSayi)
         {
-            i = i - 5;
-            Console.WriteLine(i);
+            i--5;
         }
         //Console.WriteLine(i);
         Console.ReadKey();  
