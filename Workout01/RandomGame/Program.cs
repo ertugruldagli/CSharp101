243b39a431d96e8be70e7cd453d31193785e0d6d
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-26 22:23:23 +0300

    kommit edildi

--- Workout01/RandomGame/Program.cs
+++ Workout01/RandomGame/Program.cs
@@ -6,37 +6,39 @@ internal class Program
     {
         int randomSayi, tahminSayi;
 
-        Random rnd= new Random();
-        randomSayi=rnd.Next(5);
+        Random rnd= new Random(); // random sınıfından random nesnesi üretilir
+        randomSayi=rnd.Next(1,200);  //1 ve 200 arasında bir random sayısı değişkene atılır
 
         
-
-        for (int i = 0; i <= 10; i++)
+        for (int i = 0; i <= 10; i++)         // 10 defa da tahmin etmek için for döngüsünün limiti 10 ile sınırlandırılır.
         {
             Console.WriteLine("Bir sayi tahmin ediniz: ");
-            tahminSayi = Convert.ToInt32(Console.ReadLine());
+            tahminSayi = Convert.ToInt32(Console.ReadLine()); // kullanıcıdan tahmin edilmesi istenilen sayı değişkene girilir.
 
-            if (randomSayi==tahminSayi)
+            if (randomSayi!=tahminSayi)     // girilen sayi random sayiya eşite değilse +++
             {
-                Console.WriteLine("Tebrikler tahmin ettiniz!!");
-                break;
+        
+                if (randomSayi > tahminSayi)  //++ if ile yönlendirmeler yapılarak tahmin etmesi kolaylaştırılır.
+                {
+                    Console.WriteLine("küçük değer girdiniz!");
+                }
+                else
+                {
+                    Console.WriteLine("büyük değer girdiniz!");
+                }
             }
             else 
             {
-                Console.WriteLine(" YANLIS!! Tekrar deneyin");
+                Console.BackgroundColor = ConsoleColor.Green;  //arka plan rengini yeşil yapar
+                Console.WriteLine("Tebrikler tahmin ettiniz!!");
+                break;                                              // tahmin doğru olduğu zaman döngü burada kırılır ve çıkış yapılır.
             }
- 
+            
         }
-        
+        Console.BackgroundColor = ConsoleColor.Red; //arkad plan rengini kırmızı yapar
+        Console.WriteLine("HAK DOLDU!");
 
-
-
-
-
-
-
-
-
+       
 
         Console.ReadKey();
     }
