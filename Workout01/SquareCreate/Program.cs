6a9cd5dac143a6698c28748e01e7536eb8d59ed9
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-27 08:50:42 +0300

    c

--- Workout01/SquareCreate/Program.cs
+++ Workout01/SquareCreate/Program.cs
@@ -2,32 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        int k, i;
-        
-        Console.WriteLine("Bir deger giriniz: ");
-        int deger=Convert.ToInt32(Console.ReadLine());
-
-        for ( i = 1; i < deger; i++)
-        {
-            Console.Write("*");
-        }
-        Console.WriteLine();
-        for (k = 0; k < deger - 2; k++)
-        {
-            Console.Write("* ");
-            for (int j = 0; j < deger; j++)
-            {
-                Console.Write("  ");
-            }
-            // sağ kenarı oluşturma
-            Console.Write("*");
-            Console.WriteLine();
-        }
-
-        for (int n = 0; n < deger; n++)
-            Console.Write("*");
-
-        Console.ReadKey();
+       //devamı gelecek
     }
         
 }
