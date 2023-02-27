119f28d3ca0950d73e7a593ecd0b89a16e92693a
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-27 23:22:45 +0300

    c

--- WorkOut02/EmeklilikHesaplama/Program.cs
+++ WorkOut02/EmeklilikHesaplama/Program.cs
@@ -1,56 +1,63 @@
-﻿internal class Program
+﻿using System.Reflection;
+
+internal class Program
 {
+
+    private static void Emekli(string gender, int prim)
+    {
+        int Gun, kalanYil, kalanAy, kalanGun, yilHesapla;
+       
+        if (gender == "K" || gender  == "k")
+        {
+            if (prim >= 7200)
+            {
+                Console.WriteLine("Prim gününüz doldu, emekli işlemlerinize başlayabilirsiniz!");
+            }
+            else
+            {
+                 Gun = 7200 - prim;
+                 kalanYil = (Gun / 360);
+                 yilHesapla = Gun - (kalanYil * 360);
+                 kalanAy = ((Gun - (kalanYil * 360)) / 30);
+                 kalanGun = yilHesapla - (kalanAy * 30);
+
+                Console.WriteLine("Kalan prim gün sayınız: {0}", Gun + "\n");
+                Console.WriteLine("Emekliliğine {0} yıl {1} ay {2} gün kaldı", kalanYil, kalanAy, kalanGun);
+
+
+            }
+        }
+        else if (gender == "E" || gender == "e")
+        {
+            if (prim >= 9000)
+            {
+                Console.WriteLine("Prim gününüz doldu,emekli işlemlerinize başlayabilirsiniz!");
+            }
+            else
+            {
+                 Gun = 9000 - prim;
+                 kalanYil = (Gun / 360);
+                 yilHesapla = Gun - (kalanYil * 360);
+                 kalanAy = ((Gun - (kalanYil * 360)) / 30);
+                 kalanGun = yilHesapla - (kalanAy * 30);
+            
+                Console.WriteLine("Kalan prim gün sayınız: {0}", Gun + "\n");
+                Console.WriteLine("Emekliliğine {0} yıl {1} ay {2} gün kaldı", kalanYil, kalanAy, kalanGun);
+
+
+            }
+
+        }
+
+    }
     private static void Main(string[] args)
     {
-        string cinsiyet;
-        int prim;
-
         Console.WriteLine("Cinsiyet belirtiniz; K / E....: ");
-        cinsiyet= Console.ReadLine();
+        string gender = Console.ReadLine();
 
         Console.WriteLine("calisma prim günü  sayisini giriniz: ");
-        prim = Convert.ToInt32(Console.ReadLine());
+        int prim = Convert.ToInt32(Console.ReadLine());
 
-        if (cinsiyet=="K" || cinsiyet=="k")
-        {
-            if (prim>=7200)
-            {
-                Console.WriteLine("Prim gününüz doldu, emeklisiniz!");
-            }
-            else
-            {
-                int Gun = 7200 - prim;
-                int kalanYil = Gun / 360;
-                Console.WriteLine("Kalan yıl: " + kalanYil);
-                int kalanAy = kalanYil / 12;
-                Console.WriteLine("Kalan ay: " + kalanAy);
-                int kalanGun = kalanAy / 30;
-                Console.WriteLine("Kalan gun: " + kalanGun);
-
-            }
-        }
-        else if  (cinsiyet=="E"|| cinsiyet =="e"){
-            if (prim>=9000)
-            {
-                Console.WriteLine("Prim gününüz doldu, emeklisiniz!");
-            }
-            else
-            {
-                int Gun = 9000 - prim;
-                int kalanYil = (Gun / 360);
-                int yilHesapla = Gun - (kalanYil * 360);
-                int kalanAy = ((Gun - (kalanYil * 360)) / 30);
-                int kalanGun = yilHesapla - (kalanAy * 30);
-
-                Console.WriteLine(Gun);
-                Console.WriteLine(kalanYil);
-                Console.WriteLine(kalanAy);
-                Console.WriteLine(kalanGun);
-               
-
-            }
-
-        }
-       
+        Emekli(gender,prim);
     }
 }
