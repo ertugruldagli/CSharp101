a45fbc5fa36a1bb44907d2bc8a2cee22681fa060
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-27 21:59:55 +0300

    commit

--- WorkOut02/EmeklilikHesaplama/Program.cs
+++ WorkOut02/EmeklilikHesaplama/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string cinsiyet;
+        int prim;
+
+        Console.WriteLine("Cinsiyet belirtiniz; K / E....: ");
+        cinsiyet= Console.ReadLine();
+
+        Console.WriteLine("calisma prim günü  sayisini giriniz: ");
+        prim = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+        if (cinsiyet=="K" || cinsiyet=="k")
+        {
+            if (prim>=7200)
+            {
+                Console.WriteLine("Prim gününüz doldu, emeklisiniz!");
+            }
+            else
+            {
+
+            }
+        }
+        else if  (cinsiyet=="E"|| cinsiyet =="e"){
+            if (prim>=9000)
+            {
+                Console.WriteLine("Prim gününüz doldu, emeklisiniz!");
+            }
+
+        }
+       
+    }
+}
