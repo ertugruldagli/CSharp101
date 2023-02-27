835749dd4b6d02a0fe47aed6dce0dbb894dcb3b8
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-02-27 23:08:19 +0300

    c

--- WorkOut02/EmeklilikHesaplama/Program.cs
+++ WorkOut02/EmeklilikHesaplama/Program.cs
@@ -11,11 +11,6 @@
         Console.WriteLine("calisma prim günü  sayisini giriniz: ");
         prim = Convert.ToInt32(Console.ReadLine());
 
-
-
-
-
-
         if (cinsiyet=="K" || cinsiyet=="k")
         {
             if (prim>=7200)
@@ -24,6 +19,13 @@
             }
             else
             {
+                int Gun = 7200 - prim;
+                int kalanYil = Gun / 360;
+                Console.WriteLine("Kalan yıl: " + kalanYil);
+                int kalanAy = kalanYil / 12;
+                Console.WriteLine("Kalan ay: " + kalanAy);
+                int kalanGun = kalanAy / 30;
+                Console.WriteLine("Kalan gun: " + kalanGun);
 
             }
         }
@@ -31,6 +33,21 @@
             if (prim>=9000)
             {
                 Console.WriteLine("Prim gününüz doldu, emeklisiniz!");
+            }
+            else
+            {
+                int Gun = 9000 - prim;
+                int kalanYil = (Gun / 360);
+                int yilHesapla = Gun - (kalanYil * 360);
+                int kalanAy = ((Gun - (kalanYil * 360)) / 30);
+                int kalanGun = yilHesapla - (kalanAy * 30);
+
+                Console.WriteLine(Gun);
+                Console.WriteLine(kalanYil);
+                Console.WriteLine(kalanAy);
+                Console.WriteLine(kalanGun);
+               
+
             }
 
         }
