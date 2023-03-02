c43b7932262aba6e92389e6d8c86be86a2d12e4e
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-02 16:21:54 +0300

    commit the dailyWorkout

---
+++ WorkOut02/Calculations/Program.cs
@@ -0,0 +1,17 @@
+﻿using Calculations;
+
+NewMethod();
+
+static void NewMethod()
+{
+    Console.WriteLine("Birinci sayiyi giriniz: ");
+    int sayi1=Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("İkinvi sayiyi giriniz: ");
+    int sayi2=Convert.ToInt32(Console.ReadLine());  
+
+
+    clsCalculatorMethods test1=  new clsCalculatorMethods();
+
+    test1.Dortislem(sayi1,sayi2);
+}
---
+++ WorkOut02/Calculations/clsCalculatorMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations 
+{
+    internal class clsCalculatorMethods
+    {
+       public static int topla=0,carp;
+       public static  double avarage, bolme;
+
+        public  void Dortislem(int s1,int s2)
+        {
+            
+            topla = s1 + s2;
+            carp= s1 * s2;
+            bolme= s1 / s2;
+            avarage = topla / 2;
+
+           Console.WriteLine($"Toplami: {topla}\n Caprma: {carp}\n bolme: {bolme}\n ortalamasi: {avarage}");
+
+            
+        }
+        
+
+     
+
+    }
+}
