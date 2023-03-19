5773862b7330c38841a6ef68c2cddfbc5dab001a
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-19 15:00:14 +0300

    some changes

--- WinF101/WO03/Form1.Designer.cs
+++ WinF101/WO03/Form1.Designer.cs
@@ -31,11 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.tBoxSaat = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cBoxType = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.liBoxHesapla = new System.Windows.Forms.ListBox();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,10 +53,11 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "EXİT";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // tBoxSaat
             // 
-            this.tBoxSaat.Location = new System.Drawing.Point(112, 50);
+            this.tBoxSaat.Location = new System.Drawing.Point(116, 73);
             this.tBoxSaat.Name = "tBoxSaat";
             this.tBoxSaat.Size = new System.Drawing.Size(152, 20);
             this.tBoxSaat.TabIndex = 1;
@@ -64,60 +67,89 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Location = new System.Drawing.Point(16, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Saat Giriniz: ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // comboBox1
+            // cBoxType
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(112, 91);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(152, 21);
-            this.comboBox1.TabIndex = 3;
+            this.cBoxType.FormattingEnabled = true;
+            this.cBoxType.Items.AddRange(new object[] {
+            "Otomobil",
+            "Minibus",
+            "Ticari"});
+            this.cBoxType.Location = new System.Drawing.Point(116, 114);
+            this.cBoxType.Name = "cBoxType";
+            this.cBoxType.Size = new System.Drawing.Size(152, 21);
+            this.cBoxType.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Location = new System.Drawing.Point(16, 119);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(74, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "Araç Tipi:";
             // 
-            // liBoxHesapla
-            // 
-            this.liBoxHesapla.FormattingEnabled = true;
-            this.liBoxHesapla.Location = new System.Drawing.Point(49, 39);
-            this.liBoxHesapla.Name = "liBoxHesapla";
-            this.liBoxHesapla.Size = new System.Drawing.Size(148, 134);
-            this.liBoxHesapla.TabIndex = 5;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label3.Location = new System.Drawing.Point(33, 0);
+            this.label3.Location = new System.Drawing.Point(60, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(180, 25);
+            this.label3.Size = new System.Drawing.Size(137, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Ödenecek Tutar";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.panel1.Controls.Add(this.liBoxHesapla);
+            this.panel1.Controls.Add(this.lblMessage);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Location = new System.Drawing.Point(15, 201);
+            this.panel1.Location = new System.Drawing.Point(15, 227);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(243, 185);
             this.panel1.TabIndex = 7;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnHesapla.Location = new System.Drawing.Point(88, 154);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(110, 27);
+            this.btnHesapla.TabIndex = 8;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label4.Location = new System.Drawing.Point(60, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ÖDEME SAYFASI";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(98, 83);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 10;
             // 
             // frmOtotpark
             // 
@@ -125,9 +157,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(292, 514);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnHesapla);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cBoxType);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tBoxSaat);
             this.Controls.Add(this.button1);
@@ -146,11 +180,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox tBoxSaat;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cBoxType;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox liBoxHesapla;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblMessage;
     }
 }
 
--- WinF101/WO03/Form1.cs
+++ WinF101/WO03/Form1.cs
@@ -21,5 +21,41 @@ namespace WO03
         {
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int fiyat=0;
+            string select=cBoxType.Text;
+            string saat=tBoxSaat.Text;
+
+            switch (select)
+            {
+                case "Otomobil":
+                    fiyat+= fiyat+10;
+                    lblMessage.Text= $"Odenecek tutar: {fiyat.ToString()}";
+                    break;
+
+                case "Minibus":
+                    fiyat += 15;
+                    lblMessage.Text = $"Odenecek tutar: {fiyat.ToString()}";
+                    break;
+
+                case "Ticari":
+                    fiyat += 20;
+                    lblMessage.Text = $"Odenecek tutar: {fiyat.ToString()}";
+                    break;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
