5f2610394af8c62112d108ed7169183c90d843f3
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-19 13:54:17 +0300

    the end

--- WinF101/Wo02/Form1.Designer.cs
+++ WinF101/Wo02/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Wo02
 {
-    partial class Form1
+    partial class frmTwoCBoxes
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cBoxConrol = new System.Windows.Forms.ComboBox();
+            this.cBoxListele = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnExit.Location = new System.Drawing.Point(227, 352);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(65, 33);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // cBoxConrol
+            // 
+            this.cBoxConrol.FormattingEnabled = true;
+            this.cBoxConrol.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"});
+            this.cBoxConrol.Location = new System.Drawing.Point(78, 84);
+            this.cBoxConrol.Name = "cBoxConrol";
+            this.cBoxConrol.Size = new System.Drawing.Size(111, 21);
+            this.cBoxConrol.TabIndex = 1;
+            this.cBoxConrol.SelectedIndexChanged += new System.EventHandler(this.btnListele_Click);
+            // 
+            // cBoxListele
+            // 
+            this.cBoxListele.FormattingEnabled = true;
+            this.cBoxListele.Location = new System.Drawing.Point(78, 185);
+            this.cBoxListele.Name = "cBoxListele";
+            this.cBoxListele.Size = new System.Drawing.Size(127, 21);
+            this.cBoxListele.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(7, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seçiniz: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(102, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Listelenen";
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.Color.Green;
+            this.btnListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListele.ForeColor = System.Drawing.Color.White;
+            this.btnListele.Location = new System.Drawing.Point(210, 78);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(65, 31);
+            this.btnListele.TabIndex = 5;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = false;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // frmTwoCBoxes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(304, 397);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cBoxListele);
+            this.Controls.Add(this.cBoxConrol);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmTwoCBoxes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TwoComboBoxes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ComboBox cBoxConrol;
+        private System.Windows.Forms.ComboBox cBoxListele;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnListele;
     }
 }
 
--- WinF101/Wo02/Form1.cs
+++ WinF101/Wo02/Form1.cs
@@ -10,11 +10,35 @@ using System.Windows.Forms;
 
 namespace Wo02
 {
-    public partial class Form1 : Form
+    public partial class frmTwoCBoxes : Form
     {
-        public Form1()
+        public frmTwoCBoxes()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            int t = Convert.ToInt32(cBoxConrol.Text);
+            
+
+            for (int i = t; i < ; i++)
+            {
+               
+                cBoxListele.Items.Add(i);
+            }
+            
+
+
+
+
+
+
         }
     }
 }
