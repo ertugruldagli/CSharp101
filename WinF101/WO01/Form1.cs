f55a6a4dac85d2c2adadffade5a9db17a832a853
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-19 19:51:41 +0300

    some changes

--- WinF101/WO01/Form1.cs
+++ WinF101/WO01/Form1.cs
@@ -17,26 +17,40 @@ namespace WO01
             InitializeComponent();
         }
 
+
+        #region Method
+        public void TersYazma() //işlem yapılam yer
+        {
+            string ters = tBoxString.Text; //string texten alındı
+
+            tBoxOutput.Clear();
+            
+            for (int i = ters.Length - 1; i >= 0; i--) //string uzunlupuna göre tersten döngüye girildi
+            {
+                
+                tBoxOutput.Text += ters[i];    //ters alınan stringin herbir karakterini  output textine yazdırıldı
+            }
+
+        }
+        #endregion
+
+        #region Çıkış Butonu
         private void button1_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
+        #endregion
 
+        #region İşlemler
         private void btnYazdir_Click(object sender, EventArgs e)
         {
+        
             TersYazma(); //Method oluşturarak yapıldı.
+            
         }
-
-        public void TersYazma() //işlem yapılam yer
-        {
-             string ters=tBoxString.Text; //string texten alındı
-
-            for (int i=ters.Length-1;i>=0;i--) //string uzunlupuna göre tersten döngüye girildi
-            {
-                tBoxOutput.Text += ters[i];    //ters alınan stringin herbir karakterini  output textine yazdırıldı
-            }
+        #endregion
 
 
-        }
+
     }
 }
--- WinF101/Wo02/Form1.cs
+++ WinF101/Wo02/Form1.cs
@@ -12,6 +12,8 @@ namespace Wo02
 {
     public partial class frmTwoCBoxes : Form
     {
+
+        #region Method
         public void DegerAtama()  // comboBox içerisine  manuel girmek yerine method oluşturularak otomatik olarak girildi.
         {
             for (int i = 0; i <= 20; i++)
@@ -19,6 +21,7 @@ namespace Wo02
                 cBoxConrol.Items.Add(i);
             }
         }
+        #endregion
 
         public frmTwoCBoxes()
         {
@@ -27,29 +30,34 @@ namespace Wo02
 
             DegerAtama();// method form içerisinden çağırıldı.
         }
-
+        #region Exit Butonu
         private void btnExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
+        #endregion
 
+        #region İslemler
         private void btnListele_Click(object sender, EventArgs e)
         {
             cBoxListele.Items.Clear(); //butona bastıktan sonra temizlendi.
 
             int t = Convert.ToInt32(cBoxConrol.Text);
-            
 
-            for (int i = t+1; i <= t+10; i++)  //seçilen sayının bir fazlasından alarak seçilen değerin 10 fazlasına kadar getirdi.
+
+            for (int i = t + 1; i <= t + 10; i++)  //seçilen sayının bir fazlasından alarak seçilen değerin 10 fazlasına kadar getirdi.
             {
-               
+
                 cBoxListele.Items.Add(i); // seçilen değerleri yeni cboxa yazdırdı.
             }
 
 
         }
-            
+        #endregion
 
-     
+
+
+
+
     }
 }
