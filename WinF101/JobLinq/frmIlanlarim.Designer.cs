e0da3f7a5a33934d096384ba6ecda09b8f2b87fd
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-04-18 12:04:48 +0300

    some changes

--- WinF101/JobLinq/frmIlanlarim.Designer.cs
+++ WinF101/JobLinq/frmIlanlarim.Designer.cs
@@ -41,6 +41,28 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.tBoxIlanSirketId = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.tBoxIlanDepartman = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.cBoxIlanSehir = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tBoxIlanTecrube = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tBoxIlanEgitim = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tBoxIlanYabanciDil = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tBoxIlanCalisma = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tBoxIlanPozisyon = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tBoxIlanDetay = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgridIlanlarim)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
@@ -48,14 +70,15 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgridIlanlarim
             // 
             this.dgridIlanlarim.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgridIlanlarim.Location = new System.Drawing.Point(196, 12);
+            this.dgridIlanlarim.Location = new System.Drawing.Point(179, 12);
             this.dgridIlanlarim.Name = "dgridIlanlarim";
-            this.dgridIlanlarim.Size = new System.Drawing.Size(575, 426);
+            this.dgridIlanlarim.Size = new System.Drawing.Size(575, 491);
             this.dgridIlanlarim.TabIndex = 0;
             // 
             // groupBox1
@@ -182,11 +205,236 @@
             this.btnExit.Text = "EXIT";
             this.btnExit.UseVisualStyleBackColor = true;
             // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.tBoxIlanPozisyon);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.tBoxIlanDetay);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Controls.Add(this.tBoxIlanYabanciDil);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.tBoxIlanCalisma);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.tBoxIlanTecrube);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.tBoxIlanEgitim);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.cBoxIlanSehir);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.btnAra);
+            this.groupBox2.Controls.Add(this.tBoxIlanSirketId);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.btnSil);
+            this.groupBox2.Controls.Add(this.tBoxIlanDepartman);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.btnUpdate);
+            this.groupBox2.Location = new System.Drawing.Point(760, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(258, 492);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(40, 405);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(86, 34);
+            this.btnAra.TabIndex = 11;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            // 
+            // tBoxIlanSirketId
+            // 
+            this.tBoxIlanSirketId.Location = new System.Drawing.Point(115, 18);
+            this.tBoxIlanSirketId.Name = "tBoxIlanSirketId";
+            this.tBoxIlanSirketId.Size = new System.Drawing.Size(113, 20);
+            this.tBoxIlanSirketId.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(6, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 16);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "SirketID: ";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(143, 405);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(86, 34);
+            this.btnSil.TabIndex = 10;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // tBoxIlanDepartman
+            // 
+            this.tBoxIlanDepartman.Location = new System.Drawing.Point(115, 54);
+            this.tBoxIlanDepartman.Name = "tBoxIlanDepartman";
+            this.tBoxIlanDepartman.Size = new System.Drawing.Size(113, 20);
+            this.tBoxIlanDepartman.TabIndex = 2;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label9.Location = new System.Drawing.Point(6, 58);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(80, 16);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Departman: ";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(91, 445);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(86, 34);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // cBoxIlanSehir
+            // 
+            this.cBoxIlanSehir.FormattingEnabled = true;
+            this.cBoxIlanSehir.Location = new System.Drawing.Point(113, 258);
+            this.cBoxIlanSehir.Name = "cBoxIlanSehir";
+            this.cBoxIlanSehir.Size = new System.Drawing.Size(115, 21);
+            this.cBoxIlanSehir.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label10.Location = new System.Drawing.Point(11, 263);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(44, 16);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Sehir: ";
+            // 
+            // tBoxIlanTecrube
+            // 
+            this.tBoxIlanTecrube.Location = new System.Drawing.Point(113, 91);
+            this.tBoxIlanTecrube.Name = "tBoxIlanTecrube";
+            this.tBoxIlanTecrube.Size = new System.Drawing.Size(113, 20);
+            this.tBoxIlanTecrube.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(7, 95);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 16);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Tecrube: ";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tBoxIlanEgitim
+            // 
+            this.tBoxIlanEgitim.Location = new System.Drawing.Point(113, 127);
+            this.tBoxIlanEgitim.Name = "tBoxIlanEgitim";
+            this.tBoxIlanEgitim.Size = new System.Drawing.Size(113, 20);
+            this.tBoxIlanEgitim.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(5, 131);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 16);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "EgitimSeviyesi: ";
+            // 
+            // tBoxIlanYabanciDil
+            // 
+            this.tBoxIlanYabanciDil.Location = new System.Drawing.Point(113, 158);
+            this.tBoxIlanYabanciDil.Name = "tBoxIlanYabanciDil";
+            this.tBoxIlanYabanciDil.Size = new System.Drawing.Size(113, 20);
+            this.tBoxIlanYabanciDil.TabIndex = 29;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label11.Location = new System.Drawing.Point(4, 162);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(82, 16);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "Yabanci Dil: ";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tBoxIlanCalisma
+            // 
+            this.tBoxIlanCalisma.Location = new System.Drawing.Point(113, 194);
+            this.tBoxIlanCalisma.Name = "tBoxIlanCalisma";
+            this.tBoxIlanCalisma.Size = new System.Drawing.Size(113, 20);
+            this.tBoxIlanCalisma.TabIndex = 30;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label12.Location = new System.Drawing.Point(7, 195);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(55, 16);
+            this.label12.TabIndex = 31;
+            this.label12.Text = "Ç. Şekli:";
+            // 
+            // tBoxIlanPozisyon
+            // 
+            this.tBoxIlanPozisyon.Location = new System.Drawing.Point(113, 226);
+            this.tBoxIlanPozisyon.Name = "tBoxIlanPozisyon";
+            this.tBoxIlanPozisyon.Size = new System.Drawing.Size(113, 20);
+            this.tBoxIlanPozisyon.TabIndex = 33;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label13.Location = new System.Drawing.Point(5, 230);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(68, 16);
+            this.label13.TabIndex = 32;
+            this.label13.Text = "Pozisyon: ";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tBoxIlanDetay
+            // 
+            this.tBoxIlanDetay.Location = new System.Drawing.Point(113, 298);
+            this.tBoxIlanDetay.Name = "tBoxIlanDetay";
+            this.tBoxIlanDetay.Size = new System.Drawing.Size(113, 20);
+            this.tBoxIlanDetay.TabIndex = 34;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label14.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label14.Location = new System.Drawing.Point(6, 299);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(73, 16);
+            this.label14.TabIndex = 35;
+            this.label14.Text = "Ilan Detay: ";
+            // 
             // frmIlanlarim
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(983, 526);
+            this.ClientSize = new System.Drawing.Size(1030, 526);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dgridIlanlarim);
             this.Name = "frmIlanlarim";
@@ -200,6 +448,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -219,5 +469,27 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox tBoxIlanSirketId;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox tBoxIlanDepartman;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ComboBox cBoxIlanSehir;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tBoxIlanPozisyon;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox tBoxIlanDetay;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox tBoxIlanYabanciDil;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tBoxIlanCalisma;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox tBoxIlanTecrube;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tBoxIlanEgitim;
+        private System.Windows.Forms.Label label7;
     }
 }
