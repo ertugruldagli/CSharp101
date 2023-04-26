3e5e32afc8a092e27591e434d2ea18b2863d5f62
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-04-26 13:46:51 +0300

    create the DersTable Layer

--- WinF101/EntityLayer/clsOgrenci.cs
+++ WinF101/EntityLayer/clsOgrenci.cs
@@ -8,5 +8,12 @@ namespace EntityLayer
 {
     internal class clsOgrenci
     {
+        public int OgrID { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Numara { get; set; }
+        public string Bolum { get; set; }
+
+
     }
 }
