eaf4a46b40bc88c84c363577995ef5e2349794bf
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-04-27 13:22:57 +0300

    create the katmanlimimari

---
+++ WinF101/DataAccessLayer/Baglanti.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Baglanti
+    {
+        public static SqlConnection connection =new SqlConnection(@"Data Source=ED-INTERN;Initial Catalog=DBKatmanliMimari;Integrated Security=True");
+    }
+}
